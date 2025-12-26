28b24d966f8029ab4c64496ccfc9cbd515f62eb1
pierraw1 <peterbacq@gmail.com>
2025-12-26 12:30:26 +0100

    events

---
+++ src/content/config.ts
@@ -0,0 +1,20 @@
+// src/content/config.ts
+import { defineCollection, z } from 'astro:content';
+
+const evenementsCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    lieu: z.string(),
+    image: z.string().optional(),
+    club: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().default(false),
+  }),
+});
+
+export const collections = {
+  'evenements': evenementsCollection,
+};
